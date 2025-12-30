fcfa16dd49989dfefa6026f6c619e42cb15a5ed6
Nikita Vlaskin <sinezeleuny@gmail.com>
2025-12-30 22:54:05 +0100

    Presents draft

---
+++ 2025/day_12/drafts/part_1.py
@@ -0,0 +1,8 @@
+# How many of the regions can fit all of the presents listed?
+
+def main():
+    with open('test.txt', 'r') as input:
+        input.readlines
+
+if __name__ == '__main__':
+    main()
